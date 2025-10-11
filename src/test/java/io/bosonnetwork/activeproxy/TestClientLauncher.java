67a69d3425f6159bc75d202db7f7ad288c21af16
Jingyu <jingyu.niu@gmail.com>
2025-10-11 23:11:33 +0800

    Improve the configuration implementation for easy to use and less dependency

--- src/test/java/io/bosonnetwork/activeproxy/TestClientLauncher.java
+++ src/test/java/io/bosonnetwork/activeproxy/TestClientLauncher.java
@@ -7,14 +7,16 @@ import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 
 import io.bosonnetwork.utils.Json;
+import io.bosonnetwork.utils.vertx.VertxFuture;
 
 public class TestClientLauncher {
+	private static Vertx vertx;
 	private static ActiveProxyClient client;
 
 	private static Configuration loadConfig() throws Exception{
 		try (InputStream s = TestClientLauncher.class.getClassLoader().getResourceAsStream("testConfig.yaml")) {
 			Map<String, Object> map = Json.yamlMapper().readValue(s, Json.mapType());
-			return new Configuration(map);
+			return Configuration.fromMap(map);
 		} catch (Exception e) {
 			System.err.println("Failed to load configuration file: " + e.getMessage());
 			throw e;
@@ -25,11 +27,13 @@ public class TestClientLauncher {
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 			if (client != null) {
 				System.out.println("Shutting down client...");
-				client.stop().join();
+				client.stop()
+						.thenCompose(v -> VertxFuture.of(vertx.close()))
+						.join();
 			}
 		}));
 
-		Vertx vertx = Vertx.vertx(new VertxOptions()
+		vertx = Vertx.vertx(new VertxOptions()
 				.setWorkerPoolSize(1)
 				.setEventLoopPoolSize(1)
 				.setPreferNativeTransport(true));
